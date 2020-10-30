75f446c78f5c791367aafe84a42144188461e88f
farrukhpashazadeh <70469129+farrukhpashazadeh@users.noreply.github.com>
2020-10-30 14:30:55 +0400

    js added

---
+++ main.js
@@ -0,0 +1,14 @@
+//I will change the code below to make counter up trigger when scroll to the element
+//or it is possible to add the CounterUp and Waypoints jQuery plugins here
+
+for (let i = 0; i<453; i++) {
+    document.getElementById('counter').innerHTML = i;
+}
+
+for (let i = 0; i < 53; i++) {
+    document.getElementById('counter2').innerHTML = i + '+';
+}
+
+for (let i = 0; i < 53; i++) {
+    document.getElementById('counter3').innerHTML = i + '+';
+}
